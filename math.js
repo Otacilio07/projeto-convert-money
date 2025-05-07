2c1620a2adf3aff032ed5fbbc353ff571b292c2a
Otacilio07 <otacilio_cesar@otmail.com>
2025-05-07 10:45:51 -0300

    aprendendo Math-- matematica

---
+++ math.js
@@ -0,0 +1,33 @@
+
+/*
+math--> matematica
+- pow(2,2) / potencia / 2,2=4
+- sqrt (25,) / raiz / quadrada de 25
+- pi
+- ceil / teto / -> arredondar pra cima 
+- floor / chão / --> arrendodar pra baixo
+- random () / numero aleatrio entre 0 e 1
+
+*/
+
+const result = Math.pow(2,3)
+console.log(result)
+
+const Resulttta = Math.sqrt(25,3)
+console.log(Resulttta)
+
+const Resultado = Math.PI
+console.log(Resultado)
+
+const Resulttt = Math.ceil(3.14)
+console.log(Resulttt)
+
+const Resultt = Math.floor(3.14)
+console.log(Resultt)
+
+const Resulta= Math.random()
+console.log(Resulta)
+
+
+
+
